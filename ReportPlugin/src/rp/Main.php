5002c85d13655fe129a96ef3fca547a7e33fa877
InkoHX <33122816+InkoHX@users.noreply.github.com>
2017-10-27 08:47:25 +0900

    Update Main.php

--- ReportPlugin/src/rp/Main.php
+++ ReportPlugin/src/rp/Main.php
@@ -1,1 +1,11 @@
 <?php
+namespace rp;
+class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener 
+{
+  public function onCommand(\pocketmine\command\CommandSender $p, \pocketmine\command\Command $c, string $l, array $a): bool
+  {
+    switch (strtolower($c->getName())) 
+    {
+    }
+  }
+}

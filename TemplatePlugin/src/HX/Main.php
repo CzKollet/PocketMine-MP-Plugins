9d57cb1ff69787515eab4a461465c36cb439bcef
InkoHX <33122816+InkoHX@users.noreply.github.com>
2017-10-26 21:07:42 +0900

    Update Main.php

--- TemplatePlugin/src/HX/Main.php
+++ TemplatePlugin/src/HX/Main.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace HX;
+
+//use
+
+class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener 
+{
+  function onEnable() 
+  {
+    $this->getLogger()->info("TemplatePlugin Load");
+  }
+}

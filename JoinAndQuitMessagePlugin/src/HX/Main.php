9ce929cf6d70846f1ba8c7b68202bfd81d46ac37
InkoHX <33122816+InkoHX@users.noreply.github.com>
2017-10-26 21:37:21 +0900

    Update Main.php

--- JoinAndQuitMessagePlugin/src/HX/Main.php
+++ JoinAndQuitMessagePlugin/src/HX/Main.php
@@ -1,1 +1,13 @@
-
+<?php
+namespace HX;
+class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener 
+{
+  function onJoinMessage(\pocketmine\event\player\PlayerJoinEvent $event) 
+  {
+    $event->setJoinMessage(null);
+  }
+  function onQuitMessage(\pocketmine\event\player\PlayerQuitEvent $ev) 
+  {
+    $event->setQuitMessage(null);
+  }
+}

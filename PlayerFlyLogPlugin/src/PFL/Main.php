e2108d6b639a809e1976acbbf0e6fd1f3fdec233
InkoHX <33122816+InkoHX@users.noreply.github.com>
2017-11-01 12:44:27 +0900

    完成しました!!

--- PlayerFlyLogPlugin/src/PFL/Main.php
+++ PlayerFlyLogPlugin/src/PFL/Main.php
@@ -1,1 +1,28 @@
 <?php
+namespace PFL;
+use pocketmine\Player;
+class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener 
+{
+  function onFlyLog(\pocketmine\event\player\PlayerToggleFlightEvent $ev)
+  {
+    $player = $ev->getPlayer();
+		if($ev->isFlying())
+    {
+			$this->Admin("§l§c【警告】§6 " . $player->getName() . " §cはFlyを使用しました");
+		} else {
+			$this->Admin("§l§c【警告】§6 " . $player->getName() . " §cはFlyを使用しました");
+		}
+  }
+  public function Admin($m) 
+  {
+    foreach($this->getServer()->getOnlinePlayers() as $player) 
+    {
+        if($player->isOp()) 
+        {
+           $player->sendMessage($m);
+	}
+    }
+    $this->getLogger()->info($m);
+  }
+}
+    

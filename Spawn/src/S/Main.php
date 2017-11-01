dce7a985c53e86c7f28b44d2840ee5a426816fb0
InkoHX <33122816+InkoHX@users.noreply.github.com>
2017-11-01 16:46:53 +0900

    多分これでOK

--- Spawn/src/S/Main.php
+++ Spawn/src/S/Main.php
@@ -1,7 +1,22 @@
 <?php
 namespace S;
 use pocketmine\Player;
+use pocketmine\level\Level;
+use pocketmine\math\Vector3;
+use pocketmine\event\entity\EntityTeleportEvent;
 class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener 
 {
-  
+  public function onMoveTP(\pocketmine\event\player\PlayerMoveEvent $ev) 
+  {
+    $player = $ev->getPlayer();
+    if ($ev->getPlayer()->getY() < 0) 
+    {
+      $x = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getX();
+      $y = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getY();
+      $z = $this->getServer()->getDefaultLevel()->getSafeSpawn()->getZ();
+      $level = $this->getServer()->getDefaultLevel();
+      $player->setLevel($level);
+      $player->teleport(new Vector3($x, $y, $z, $level));
+    }
+  }
 }

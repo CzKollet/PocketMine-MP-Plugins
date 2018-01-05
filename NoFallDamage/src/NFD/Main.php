b86750b1c48cacba976ba9a8474969635bdab8da
InkoHX <33122816+InkoHX@users.noreply.github.com>
2018-01-05 17:59:53 +0900

    fix

--- NoFallDamage/src/NFD/Main.php
+++ NoFallDamage/src/NFD/Main.php
@@ -6,7 +6,7 @@ use pocketmine\event\entity\EntityDamageEvent;
 class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener {
   function onEnable() {
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
-  )
+  }
   public function onEntityEventDamage(EntityDamageEvent $event) {
         if ($event->getCause() == EntityDamageEvent::CAUSE_VOID or $event->getCause() == EntityDamageEvent::CAUSE_FALL) {
             $event->setCancelled();
